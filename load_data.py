89f57783c6ba945b92a91161828774c39fb73108
Saba Ghaffari <33433428+sabagh1994@users.noreply.github.com>
2018-12-14 18:09:33 -0600

    code2

---
+++ load_data.py
@@ -0,0 +1,157 @@
+"""load_data module"""
+#from argparse import ArgumentParser
+import os
+import yaml
+#import params
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+
+def get_batch(param_dict,#=params.default_param_dict(),
+              shuffled=True):
+    """ uses data_batch_size, data_dir, data_mode, data_serialized """
+
+    batch_size = param_dict["data_batch_size"]
+    all_files = sorted(os.listdir(param_dict["data_dir"]))
+    nthreads = 1
+
+    # get data characteristics
+    data_char_file = os.path.join(param_dict["data_dir"], "info.yml")
+    with open(data_char_file) as infile:
+        data_dict = yaml.safe_load(infile)
+    print("### start load_data")
+    print("data class_column: " + str(data_dict['class_column']))
+    print("data num_metadata: " + str(data_dict['num_metadata']))
+    print("data num_examples: " + str(data_dict['num_examples']))
+
+    # get list of files
+    filenames = []
+    filesuffix = '.data'
+    readtype = tf.TextLineReader
+    if param_dict["data_serialized"]:
+        filesuffix = '.tfrecord'
+        readtype = tf.TFRecordReader
+    for fname in all_files:
+        if filesuffix in fname:
+            filenames.extend([param_dict["data_dir"] + fname])
+    nreaders = min(nthreads, len(filenames))
+    print("data number of datafiles: " + str(len(filenames)))
+    print("data example datafile: " + filenames[0])
+    print("data batch_size: " + str(batch_size))
+    print("data nreaders: " + str(nreaders))
+
+    # read in example
+    if shuffled:
+        # Reads multiple records in parallel from data_sources using n readers.
+        key, example = slim.parallel_reader.parallel_read(filenames,
+                                                          readtype,
+                                                          num_epochs=None,
+                                                          num_readers=nreaders,
+                                                          shuffle=True,
+                                                          dtypes=None,
+                                                          capacity=32*batch_size,
+                                                          min_after_dequeue=16*batch_size,
+                                                          seed=19850411,
+                                                          scope=None)
+    else:
+        # Reads sequentially the data_sources using the reader, doing a single pass.
+        filename_queue = tf.train.string_input_producer(filenames, shuffle=False)
+        reader = readtype()
+        key, example = reader.read(filename_queue)
+
+    #print("data raw example size: " + str(example))
+
+    # decode example into features, label and metadata
+    if param_dict["data_serialized"]:
+        parse_example = tf.parse_single_example(example,
+            features={
+                'feature_values' : tf.FixedLenFeature([data_dict['class_column']-1], tf.float32),
+                'label' : tf.FixedLenFeature([1], tf.int64),
+                'meta_values' : tf.FixedLenFeature([data_dict['num_metadata']], tf.string)
+            })
+        features = tf.cast(parse_example['feature_values'], tf.float32)
+        label = tf.cast(parse_example['label'], tf.int32)
+        metadata = tf.cast(parse_example['meta_values'], tf.string)
+    else:
+        record_defaults = [[1.0] for dim in range(data_dict['class_column']-1)]
+        record_defaults.extend([[1]])
+        record_defaults.extend([['str'] for dim in range(data_dict['num_metadata'])])
+        print("data record_defaults length: " + str(len(record_defaults)))
+        reader = tf.decode_csv(records=example, record_defaults=record_defaults,
+                               field_delim="\t")
+        #print("data size of reader: " + str(reader))
+        #tf.decode_csv() from slim.parallel_reader.parallel_read() returns tensors
+        #with <unknown> shape.
+        #This shape needs to be casted to () to be used with tf.train.batch()
+        reshaped_reader = []
+        for tensor in reader:
+            reshaped_reader.append(tf.reshape(tensor, []))
+        #print("data size of reshaped_reader: " + str(reshaped_reader))
+        features = reshaped_reader[0:data_dict['class_column']-1]
+        label = reshaped_reader[data_dict['class_column']-1:data_dict['class_column']]
+        metadata = reshaped_reader[data_dict['class_column']:(data_dict['class_column']
+                                                              +data_dict['num_metadata'])]
+    #print("data size of features: " + str(features))
+    #print("data size of label: " + str(label))
+    #print("data size of metadata: " + str(metadata))
+
+    # reformat example features
+    label = tf.squeeze(label)
+    input_size = data_dict['class_column']-1
+    if param_dict['data_mode'] == 'diff':
+        input_size = int((input_size)/2)
+        features = (tf.slice(features, [input_size], [input_size])
+                    - tf.slice(features, [0], [input_size]))
+    elif param_dict['data_mode'] == 'exp_only':
+        input_size = int((input_size)/2)
+        features = tf.slice(features, [input_size], [input_size])
+    #features.set_shape([input_size])
+    param_dict['data_input_size'] = input_size
+    print("data orig input_size: " + str(data_dict['class_column']-1))
+    print("data final input_size: " + str(input_size))
+
+    # create batch
+    if shuffled:
+        feat_b, label_b, meta_b = tf.train.shuffle_batch([features, label, metadata],
+                                                         batch_size=batch_size,
+                                                         num_threads=nthreads,
+                                                         capacity=32*batch_size,
+                                                         min_after_dequeue=16*batch_size,
+                                                         seed=19850411,
+                                                         allow_smaller_final_batch=True)
+    else:
+        feat_b, label_b, meta_b = tf.train.batch([features, label, metadata],
+                                                 batch_size=batch_size,
+                                                 num_threads=1,
+                                                 capacity=batch_size,
+                                                 allow_smaller_final_batch=True)
+
+    return feat_b, label_b, meta_b, input_size, data_dict['num_metadata'], data_dict['num_examples']
+
+
+#def load_data_test():
+#    """test for load_data module"""
+#    parser = ArgumentParser()
+#    parser = params.add_trainer_args(parser)
+#    param_dict = vars(parser.parse_args())
+#    feat_b, label_b, meta_b, input_size, nummeta, numexamps = get_batch(param_dict, param_dict["training"])
+#    print("data final input_size: " + str(input_size), file= open('printout','a'))
+#    print("data nummeta: " + str(nummeta), file= open('printout','a'))
+#    with tf.Session() as sess:
+#        # initialize the variables
+#        sess.run(tf.initialize_all_variables())
+#        # initialize the queue threads to start to shovel data
+#        coord = tf.train.Coordinator()
+#        threads = tf.train.start_queue_runners(coord=coord)
+#        print("data feat_batch, label_batch, meta_batch: ", file= open('printout','a'))
+#        for step in range(param_dict["train_max_steps"]):
+#            print("data train_step: " + str(step),file= open('printout','a'))
+#            print(step, file= open('printout','a'))
+#            print(sess.run([feat_b, label_b, meta_b]), file= open('printout','a'))
+#        # We request our child threads to stop ...
+#        coord.request_stop()
+#        # ... and we wait for them to do so before releasing the main thread
+#        coord.join(threads)
+
+#if __name__ == '__main__':
+#    load_data_test()
---
+++ params.py
@@ -0,0 +1,173 @@
+"""define parameters"""
+#from argparse import ArgumentParser
+import os
+import yaml
+
+DEFAULT_CHKPT_DIR = 'chkpts'
+DEFAULT_LOG_DIR = 'logs'
+def add_global_params(parser):
+    """define global parameters"""
+    parser.add_argument('--train_chkpt_dir', default=DEFAULT_CHKPT_DIR, type=str,
+                        help='Directory for saved model')
+    parser.add_argument('--log_dir', default=DEFAULT_LOG_DIR, type=str,
+                        help='Directory for summary and logs')
+    return parser
+
+DEFAULT_BATCH_SIZE = 6
+DEFAULT_DATA_MODE = 'all'
+DEFAULT_DATA_DIR = './'
+def add_data_params(parser):
+    """define data parameters"""
+    parser.add_argument('--data_dir', default=DEFAULT_DATA_DIR, type=str,
+                        help='Directory storing input data')
+    parser.add_argument('--data_batch_size', type=int, default=DEFAULT_BATCH_SIZE,
+                        help='Number of examples in a minibatch')
+    parser.add_argument('--data_serialized', dest='data_serialized', default=False,
+                        action='store_true', help='Default .data files or serialized .tfrecords')
+    parser.add_argument('--data_mode', type=str, default=DEFAULT_DATA_MODE,
+                        help='Which part of the data to use: "all", "diff", "exp_only"')
+    parser.add_argument('--data_batch_norm', dest='data_batch_norm', default=False,
+                        action='store_true', help='Batch normalize outputs of in layer')
+    return parser
+
+
+DEFAULT_ACTV_STR = 'sigmoid'
+DEFAULT_CONV_DEPTH = 0
+def add_conv_params(parser):
+    """define convolutional parameters"""
+    parser.add_argument('--conv_depth', type=int, default=DEFAULT_CONV_DEPTH,
+                        help='Depth of convolutional_layer')
+    parser.add_argument('--conv_actv_str', type=str, default=DEFAULT_ACTV_STR,
+                        help='Activiation function')
+    parser.add_argument('--conv_batch_norm', dest='conv_batch_norm', default=False,
+                        action='store_true', help='Batch normalize outputs of conv layer')
+    parser.add_argument('--conv_gene_pair', default=False, action="store_true",
+                        help='Do gene pair convolution')
+    return parser
+
+
+DEFAULT_DIM_STR = '5,5'
+DEFAULT_RES_BLOCK_SIZE = 0
+def add_fcs_params(parser):
+    """define fully connected layer parameters"""
+    parser.add_argument('--fcs_dimension_str', type=str, default=DEFAULT_DIM_STR,
+                        help='Hidden layers of the model')
+    parser.add_argument('--fcs_actv_str', type=str, default=DEFAULT_ACTV_STR,
+                        help='Activiation function')
+    parser.add_argument('--fcs_batch_norm', dest='fcs_batch_norm', default=False,
+                        action='store_true', help='Batch normalize outputs of fc layer')
+    parser.add_argument('--fcs_res_block_size', type=int, default=DEFAULT_RES_BLOCK_SIZE,
+                        help='Number of layers per residual block')
+    return parser
+
+
+DEFAULT_NCLASSES = 50
+def add_outlayer_params(parser):
+    """define output parameters"""
+    parser.add_argument('--out_actv_str', type=str, default=DEFAULT_ACTV_STR,
+                        help='Activiation function')
+    parser.add_argument('--out_label_count', type=int, default=DEFAULT_NCLASSES,
+                        help='Number of classes for training')
+    return parser
+
+
+DEFAULT_KL_SPARCITY = 0.2
+def add_regularization_params(parser):
+    """define regularization parameters"""
+    parser.add_argument('--reg_do_keep_prob', type=float, default=1.0,
+                        help='Keep probability for training dropout')
+    parser.add_argument('--reg_kl_sparsity', type=float, default=DEFAULT_KL_SPARCITY,
+                        help='Probability for activation of neuron')
+    parser.add_argument('--reg_kl_scale', type=float, default=0,
+                        help='Regulariztion scalar for KL activation loss')
+    parser.add_argument('--reg_l1_scale', type=float, default=0,
+                        help='Regulariztion scalar for L1 weights loss')
+    parser.add_argument('--reg_l2_scale', type=float, default=0,
+                        help='Regulariztion scalar for L2 weights loss')
+    return parser
+
+
+DEFAULT_MAX_STEPS = 1
+DEFAULT_SECS_PER_SAVE = 600
+DEFAULT_OPTIMIZER_STR = "Adam"
+DEFAULT_LEARNING_RATE = "0.01"
+def add_train_params(parser):
+    """define training parameters"""
+    parser.add_argument('--training', dest='training', default=False,
+                        action='store_true', help='Mark if training model')
+    parser.add_argument('--train_max_steps', type=int, default=DEFAULT_MAX_STEPS,
+                        help='Number of steps to run trainer')
+    parser.add_argument('--train_save_ckpt_secs', type=int, default=DEFAULT_SECS_PER_SAVE,
+                        help='Number of seconds to run trainer before saving ckpt')
+    parser.add_argument('--train_save_summ_secs', type=int, default=DEFAULT_SECS_PER_SAVE,
+                        help='Number of seconds to run trainer before saving summaries')
+    parser.add_argument('--train_optimizer_str', type=str, default=DEFAULT_OPTIMIZER_STR,
+                        help='Optimizer function')
+    parser.add_argument('--train_learning_rate', type=float, default=DEFAULT_LEARNING_RATE,
+                        help='Initial learning rate')
+    return parser
+
+
+DEFAULT_EVAL_MODE = 'stats'
+DEFAULT_EVAL_INTERVAL = 0
+def add_eval_params(parser):
+    """define evaluation parameters"""
+    parser.add_argument('--eval_run_mode', type=str, default=DEFAULT_EVAL_MODE,
+                        help='Mode: "stats", "preds", "probs"')
+    parser.add_argument('--eval_interval_secs', type=int, default=DEFAULT_EVAL_INTERVAL,
+                        help='Time between sampling the evaluation metrics')
+    return parser
+
+
+def add_trainer_args(parser):
+    """merge parameters for training"""
+    group1 = parser.add_argument_group('global parameters')
+    group1 = add_global_params(group1)
+    group2 = parser.add_argument_group('data parameters')
+    group2 = add_data_params(group2)
+    group3 = parser.add_argument_group('training parameters')
+    group3 = add_train_params(group3)
+    pass
+    group4 = parser.add_argument_group('regularization parameters')
+    group4 = add_regularization_params(group4)
+    group5 = parser.add_argument_group('convolutional_layer parameters')
+    group5 = add_conv_params(group5)
+    group6 = parser.add_argument_group('fully_connected_layer parameters')
+    group6 = add_fcs_params(group6)
+    group7 = parser.add_argument_group('output_layer parameters')
+    group7 = add_outlayer_params(group7)
+    group8 = parser.add_argument_group('evaluation and summary parameters')
+    group8 = add_eval_params(group8)
+    return parser
+
+
+def add_evaluater_args(parser):
+    """merge parameters for evaluation"""
+    group1 = parser.add_argument_group('global parameters')
+    group1 = add_global_params(group1)
+    group2 = parser.add_argument_group('data parameters')
+    group2 = add_data_params(group2)
+    group3 = parser.add_argument_group('evaluation and summary parameters')
+    group3 = add_eval_params(group3)
+    return parser
+
+def default_param_dict(parser):
+    """create param_dict with defaults"""
+    #parser = ArgumentParser()
+    parser = add_trainer_args(parser)
+    param_dict = vars(parser.parse_args())
+    return param_dict
+
+
+def param_dict_test(parser):
+    """test param module"""
+    #parser = ArgumentParser()
+    parser = add_trainer_args(parser)
+    param_dict = vars(parser.parse_args())
+    param_yml = os.path.join(param_dict["log_dir"], 'params.yml')
+    with open(param_yml, 'w') as outfile:
+        yaml.dump(param_dict, outfile, default_flow_style=False)
+
+
+#if __name__ == '__main__':
+#    param_dict_test()
